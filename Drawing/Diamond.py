3c0af0c18992a632b531f5ed7380086b428c3cdf
Gourav Lohar <55839674+Gouravlohar@users.noreply.github.com>
2023-12-28 00:24:12 +0530

    Create Diamond.py

---
+++ Drawing/Diamond.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+image=np.zeros((500,500,3))
+# to colur the background we can use below line where first is red green and vlue
+image[:]=0,255,0
+
+# 250 in x axis and 0 in y axis and 250 in x axis and 500 in y axis
+cv2.line(image,(250,0),(250,500),(0,0,255),3)
+cv2.line(image,(0,250),(500,250),(0,0,255),3)
+
+cv2.line(image,(0,250),(250,0),(0,0,255),3)
+cv2.line(image,(250,0),(500,250),(0,0,255),3)
+cv2.line(image,(0,250),(250,500),(0,0,255),3)
+cv2.line(image,(250,500),(500,250),(0,0,255),3)
+
+
+cv2.imshow("Black",image)
+cv2.waitKey(0)
